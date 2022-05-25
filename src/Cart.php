171d2ceb7dd8f78cbf88efbcbfc931f00c826aa1
nguyenquangninh1999 <nguyenquangninh456@gmail.com>
2022-05-25 15:18:32 +0700

    first commit

--- src/Cart.php
+++ src/Cart.php
@@ -27,22 +27,29 @@ class Cart
     {
         $this->session = $session;
 
-        $this->instance = self::DEFAULT_INSTANCE;
+        $this->instance(self::DEFAULT_INSTANCE);
+    }
+
+    public function instance($instance = null)
+    {
+        $instance = $instance ?: self::DEFAULT_INSTANCE;
+        return $instance;
     }
 
     public function add($id, $name = null, $qty = null, $price = null, array $options = [])
     {
 
+        // lấy ra thông tin item nhập vào
         $cartItem = $this->createCartItem($id, $name, $qty, $price, $options);
 
         $content = $this->getContent();
-
+        // kiểm tra rowID có trong session hay chưa, nếu có rồi thì cộng thêm qty (số lượng)
         if ($content->has($cartItem->rowId)) {
             $cartItem->qty += $content->get($cartItem->rowId)->qty;
         }
-
+        //lưu thông tin item vào content
         $content->put($cartItem->rowId, $cartItem);
-
+        //lưu thông tin item vào session
         $this->session->put($this->instance, $content);
 
         return $cartItem;
@@ -50,6 +57,7 @@ class Cart
 
     protected function getContent()
     {
+        // kiểm tra xem tồn tại session chưa nếu chưa thì item rỗng, có rồi thì lấy gitri của item
         $content = $this->session->has($this->instance)
             ? $this->session->get($this->instance)
             : new Collection;
@@ -59,11 +67,16 @@ class Cart
 
     private function createCartItem($id, $name, $qty, $price, array $options)
     {
+        // kiểm tra id có phải mảng hay không
         if (is_array($id)) {
+            //lấy ra mảng item
             $cartItem = CartItem::fromArray($id);
+            //thêm vào mảng 1 trường qty
             $cartItem->setQuantity($id['qty']);
         } else {
+            //lấy ra mảng item
             $cartItem = CartItem::fromAttributes($id, $name, $price, $options);
+            //thêm vào mảng 1 trường qty
             $cartItem->setQuantity($qty);
         }
 
@@ -72,6 +85,7 @@ class Cart
 
     public function content()
     {
+        // kiểm tra session có dữ liệu k
         if (is_null($this->session->get($this->instance))) {
             return new Collection([]);
         }
@@ -81,19 +95,21 @@ class Cart
 
     public function update($rowId, $qty)
     {
+        //lấy ra giá trị session có rowID
         $cartItem = $this->get($rowId);
-
+        //lấy ra dữ liệu session
         $cartItem->qty = $qty;
-
+        //lấy ra dữ liệu session
         $content = $this->getContent();
-
+        //kiểm tra qty <=0
         if ($cartItem->qty <= 0) {
+            //xóa session rowID
             $this->remove($cartItem->rowId);
             return;
         } else {
             $content->put($cartItem->rowId, $cartItem);
         }
-
+        //lưu conten vào session
         $this->session->put($this->instance, $content);
 
         return $cartItem;
@@ -101,26 +117,28 @@ class Cart
 
     public function remove($rowId)
     {
+        //lấy thông tin session rowID
         $cartItem = $this->get($rowId);
 
         $content = $this->getContent();
-
+        //xóa session rowID
         $content->pull($cartItem->rowId);
-
+        //lưu lại session sau khi xóa
         $this->session->put($this->instance, $content);
     }
 
     public function get($rowId)
     {
         $content = $this->getContent();
-
+        //lấy dữ liệu theo rowID
         return $content->get($rowId);
     }
 
     public function store($identifier)
     {
+        //lấy nội dung session
         $content = $this->getContent();
-
+        //lưu vào csdl
         DB::table(config('cart.database.table'))->insert([
             'identifier' => $identifier,
             'instance' => $this->instance ?? 'default',
@@ -130,10 +148,12 @@ class Cart
 
     public function total($decimals = null, $decimalPoint = null, $thousandSeperator = null)
     {
+        //lấy thông tin session
         $content = $this->getContent();
 
         $total = 0;
         foreach ($content as $value) {
+            //tính tổng tất cả các mặt hàng
             $total += $value->qty * $value->price;
         }
         return $total ?? 0;
@@ -141,6 +161,7 @@ class Cart
 
     public function destroy()
     {
+        //xóa tất cả session
         $this->session->remove($this->instance);
     }
 }
